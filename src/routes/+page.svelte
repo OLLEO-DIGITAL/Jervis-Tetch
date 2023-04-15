45452f5905f0e794fe6a63382591139df952ff7d
Reaper-X <paulmichaeloliva@gmail.com>
2023-04-15 14:49:24 -0400

    Adding gitignore for .vs; Added library configuration for styles along with simple test in main page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<main>
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
+
+<style lang="scss">
+@use '../lib/styles/base/colors' as colors;
+
+main {
+	background-color: colors.$primary-red;
+}
+
+</style>
+
