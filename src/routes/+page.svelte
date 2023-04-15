1a8285f2a20158479ac4065408a63657a97e166b
Carlos David Gomez <56411602+Putipaste@users.noreply.github.com>
2023-04-15 15:46:07 -0400

    #15 added canvas init function, imported onMount from svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
+<script>
+	import { onMount } from 'svelte';
+	let canvas;
+	let context;
+
+	function init() {
+		canvas = document.getElementById("mainCanvas");
+		context = canvas.getContext("2d");
+		context.fillStyle = 'hsl(211, 21%, 42%)';
+		context.fillRect(10, 10, 100, 100);
+	}
+	// onMount function
+	onMount(() => {
+		init();
+	});
+</script>
+
+<canvas id="mainCanvas" width="200" height="200" />
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
