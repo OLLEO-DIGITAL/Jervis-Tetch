5b91585c6e4114dcff0f29fdae0a9745a1ebd819
Asad Richardson <asad@hasshdigital.com>
2023-04-15 14:26:33 -0400

    #17 Added sveltepreprocessor to project to allow additional config features

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: preprocess({
+		scss: {
+			includePaths: ['./src/lib/styles']
+		}
+	}),
 };
 
 export default config;
