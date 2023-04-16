1a3ccaba8f34887510d9bea4c874644edbe531c2
Carlos David Gomez <56411602+Putipaste@users.noreply.github.com>
2023-04-15 21:50:37 -0400

    #15 filled window with canvas and added a background to the canvas/page  hsl(211, 21%, 42%)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,15 +7,13 @@
 		canvas = document.getElementById("mainCanvas");
 		context = canvas.getContext("2d");
 		context.fillStyle = "hsl(211, 21%, 42%)";
-		context.fillRect(10, 10, 100, 100);
+		context.fillRect(0, 0, 700, 700);
 	}
 	// onMount function
 	onMount(() => {
 		init();
 	});
 </script>
-
-<canvas id="mainCanvas" width="200" height="200" />
 
 <main>
 	<h1>Welcome to SvelteKit</h1>
@@ -24,11 +22,19 @@
 	</p>
 </main>
 
+<canvas id="mainCanvas" />
+
 <style lang="scss">
 	@use "base/colors" as colors;
 	@use "base/typography" as typos;
 
 	main {
 		background-color: colors.$primary-red;
+	}
+
+	#mainCanvas {
+		min-width: 700px;
+		min-height: 700px;
+		border: 1px solid red;
 	}
 </style>
